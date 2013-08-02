85fc209fe75cc1b299dc0470633259c8f0c97c04
maximaximal <mail@maximaximal.com>
2013-08-02 21:01:39 +0200

    Added documentation to the Hoverable widget.

--- include/legui/Hoverable.h
+++ include/legui/Hoverable.h
@@ -5,6 +5,9 @@
 
 namespace legui
 {
+    /**
+     * @brief This widget is hoverable (by the mouse cursor without clicking).
+     */
     class Hoverable : public Widget
     {
         public:
@@ -13,13 +16,37 @@ namespace legui
             virtual void onUpdate(float frametime);
             virtual bool onEvent(const sf::Event &e);
 
+            /**
+             * @brief Checks if the widget is currently hovered.
+             *
+             * @return The current hover state.
+             */
             bool isHovered();
+            /**
+             * @brief Immeadiate mode hook for the hover gained signal.
+             */
             bool hoverGained();
+            /**
+             * @brief Immediate mode hook for the hover lost signal.
+             */
             bool hoverLost();
 
+            /**
+             * @brief Is called when the mouse cursor enters the widget (the bounding box).
+             *
+             * @return The onHoverGained signal.
+             */
             Nano::signal<void()>& onHoverGained();
-            Nano::signal<void()>& onHoverLost();
+            /**
+             * @brief Is called when the mouse cursor leaves the widget (the bounding box).
+             *
+             * @return The onHoverLost signal.
+             */
+            Nano::signal<void()>& onHoverLost()
 
+            /**
+             * @brief Sets the current hover state.
+             */
             void setHover(bool state);
         private:
             bool m_hoverGained;
