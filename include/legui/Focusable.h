723ba29d82b8991e8a9dd315a0cc2cdb870423b4
maximaximal <mail@maximaximal.com>
2013-08-02 20:56:51 +0200

    Added documentation to the Focusable widget.

--- include/legui/Focusable.h
+++ include/legui/Focusable.h
@@ -5,6 +5,9 @@
 
 namespace legui
 {
+    /**
+     * @brief This widget is focusable (by tabbing through the widgets).
+     */
     class Focusable : public Hoverable
     {
         public:
@@ -12,13 +15,42 @@ namespace legui
 
             virtual void onUpdate(float frametime);
             virtual bool onEvent(const sf::Event &e);
+            /**
+             * @brief Checks if the widget is currently focused.
+             */
             bool isFocused();
+            /**
+             * @brief Immediate mode hook for the gained focus signal.
+             */
             bool gainedFox();
+            /**
+             * @brief Immediate mode hook for the lost focus signal.
+             */
             bool lostFocus();
+            /**
+             * @brief Is called when the focus comes to this widget.
+             *
+             * @return 
+             */
             Nano::signal<void()>& onFocusGained();
+            /**
+             * @brief Is called when the focus goes from this widget.
+             */
             Nano::signal<void()>& onFocusLost();
             
+            /**
+             * @brief Sets the next focusable widget which is selected once the "Tab" key is pressed.
+             *
+             * Does nothing by default (leaves the focus on this widget).
+             *
+             * @param next Pointer to the next focusable widget.
+             */
             void setNextFocusable(Focusable *next);
+            /**
+             * @brief Sets the focus of the widget.
+             *
+             * @param state Focus state.
+             */
             void setFocus(bool state);
         private:
             bool m_isFocused;
