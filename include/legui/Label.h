84905e894c6c60f3c5486663b7f06e246825d6d2
Max Heisinger <max@linux-w8xi.site>
2013-08-09 02:52:48 +0200

    Added some documentation to the label.

--- include/legui/Label.h
+++ include/legui/Label.h
@@ -12,6 +12,9 @@ namespace legui
     class Label : public Widget
     {
         public:
+            /**
+             * @brief The pre-defined default style of the label.
+             */
             enum Style {
                 Regular,
                 Heading1,
@@ -25,14 +28,45 @@ namespace legui
             virtual ~Label();
             
             virtual void setBoundingBox(const sf::FloatRect &box);
+            /**
+             * @brief Updates the size of the label & sets the string to fit into the bounding box if LABEL_RECT_BORDER is set to true.
+             */
             virtual void updateSize();
 
+            /**
+             * @brief Sets the displayed string of the widget.
+             *
+             * @param text The string to display.
+             */
             virtual void setString(const sf::String &text);
+            /**
+             * @brief Sets the used font of the text.
+             *
+             * @param font The font.
+             */
             virtual void setFont(const sf::Font &font);
+            /**
+             * @brief Sets an SFML-Style to the font. 
+             *
+             * @param style SFML style.
+             */
             virtual void setFontStyle(sf::Text::Style style);
+            /**
+             * @brief Sets a predefined style to the font.
+             *
+             * @param style The style.
+             */
             virtual void setStyle(Label::Style style);
+            /**
+             * @brief Sets the character size of the font.
+             *
+             * @param size THe character size.
+             */
             virtual void setCharacterSize(unsigned int size);
             const sf::String& getString(); 
+            /**
+             * @brief If LABEL_RECT_BORDER is set, this si the string in the visible area.
+             */
             const sf::String& getVisibleString(); 
             const sf::Font* getFont();
             sf::Text::Style getFontStyle();
