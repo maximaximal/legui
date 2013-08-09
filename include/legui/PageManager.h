ac5cd9cf6e4725af48b7a6f52552985f6167624e
maximaximal <mail@maximaximal.com>
2013-08-10 01:50:53 +0200

    Added the definition of the PageManager.

---
+++ include/legui/PageManager.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <legui/Container.h>
+
+namespace legui
+{
+    /**
+     * @brief Manages gui pages and their required updates.
+     *
+     * The gui is built ontop of pages, which are stacked like a 
+     * stack of paper. You push new pages ontop of it and pop it afterwards.
+     *
+     * If there is a new page without the possibility to go back, then you 
+     * also are allowed to delete a specific page, to replace the current one, or
+     * to delete the previous one.
+     */
+    class PageManager : public Container
+    {
+        public:
+            PageManager();
+            virtual ~Page();
+
+            virtual void onUpdate(float frametime);
+            virtual void onEvent(const sf::Event &e);
+            virtual void setBoundingBox(const sf::FloatRect &box);
+            virtual void updateSize();
+            
+            void push(Container *container);
+            void replaceCurrent(Container *container);
+            void popUnder();
+            void pop(std::size_t page);
+            void pop();
+            Container* top();
+            Container* get(std::size_t page);
+            std::size_t getCurrentPageId();
+        protected:
+            virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+        private:
+            std::size_t m_currentPage;
+            bool m_screenSizeChanged;
+            bool m_updateSizeRequested;
+    };
+}
