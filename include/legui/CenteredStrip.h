ce117016c80c11b7c6df7834f6c2afcf62ce6500
maximaximal <mail@maximaximal.com>
2013-08-11 22:37:45 +0200

    Added a centered strip widget layouter.
    This layout centers all containing widgets.

---
+++ include/legui/CenteredStrip.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <legui/Container.h>
+
+namespace legui
+{
+    /**
+     * @brief Centers the containing elements in the middle of its bounding box.
+     *
+     * The element sizes aren't changed, only their positions!
+     */
+    class CenteredStrip : public Container
+    {
+        public:
+            CenteredStrip(Container *parent = 0);
+            virtual ~CenteredStrip()
+            /**
+             * @brief Only alignes the elements, doesn't give them a new size!
+             */
+            virtual void setBoundingBox(const sf::FloatRect &box);
+    };
+}
---
+++ src/CenteredStrip.cpp
@@ -0,0 +1,27 @@
+#include <legui/CenteredStrip.h>
+
+namespace legui
+{
+    CenteredStrip::CenteredStrip(Container *parent)
+        : Container(parent)
+    {
+        
+    }
+    CenteredStrip::~CenteredStrip()
+    {
+    
+    }
+    void CenteredStrip::setBoundingBox(const sf::FloatRect &box)
+    {
+        sf::FloatRect middle;
+        float y = 0;
+        for(auto &it : m_widgets)
+        {
+            middle = it->getBoundingBox();
+            middle.left = box.left + box.width / 2 - middle.width / 2;
+            middle.top = y;
+            it->setBoundingBox()
+            y += middle.height;
+        }
+    }
+}
