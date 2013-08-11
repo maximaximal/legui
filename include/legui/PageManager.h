52e692e131575f7e0b5169dec03d03821073d246
maximaximal <mail@maximaximal.com>
2013-08-11 22:17:46 +0200

    Added documentation to the page manager.

--- include/legui/PageManager.h
+++ include/legui/PageManager.h
@@ -25,13 +25,51 @@ namespace legui
             virtual void setBoundingBox(const sf::FloatRect &box);
             virtual void updateSize();
             
+            /**
+             * @brief Pushes a new page ontop of the stack & makes it active.
+             *
+             * @param container The new page.
+             */
             void push(Container *container);
+            /**
+             * @brief Replaces & deletes the current page.
+             *
+             * @param container The new page.
+             */
             void replaceCurrent(Container *container);
+            /**
+             * @brief Deletes the page under the current one.
+             */
             void popUnder();
+            /**
+             * @brief Deletes the specified page.
+             *
+             * @param page The page ID to be deleted.
+             */
             void pop(std::size_t page);
+            /**
+             * @brief Pops (deletes) the page on the top & makes the page below the new current page.
+             */
             void pop();
+            /**
+             * @brief Returns the current page.
+             *
+             * @return The current page.
+             */
             Container* top();
+            /**
+             * @brief Returns the specified page.
+             *
+             * @param page The page id of the requested page.
+             *
+             * @return The requested page.
+             */
             Container* get(std::size_t page);
+            /**
+             * @brief Gets the current page id (of the page ontop).
+             *
+             * @return The currenz page id.
+             */
             std::size_t getCurrentPageId();
         protected:
             virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
