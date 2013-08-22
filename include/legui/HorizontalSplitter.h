4b65ad749286339e14ebdd9145089b8b2deb8f7a
maximaximal <mail@maximaximal.com>
2013-08-22 02:52:10 +0200

    Added vertical padding to the horizontal splitter.

--- include/legui/HorizontalSplitter.h
+++ include/legui/HorizontalSplitter.h
@@ -23,12 +23,27 @@ namespace legui
              */
             void setPadding(float padding = 0);
             /**
+             * @brief Sets the padding between the internal widgets.
+             *
+             * The padding is applied between the containing elements (upper & lower, between).
+             *
+             * @param Vpadding The padding in pixels.
+             */
+            void setVPadding(float Vpadding = 0);
+            /**
              * @brief Returns the padding between the internal widgets.
              *
              * @return The padding in pixels.
              */
             float getPadding();
+            /**
+             * @brief Returns the vertical padding between the internal widgets.
+             *
+             * @return The padding in pixels.
+             */
+            float getVPadding();
         private:
             float m_padding;
+            float m_Vpadding;
     };
 }
--- src/HorizontalSplitter.cpp
+++ src/HorizontalSplitter.cpp
@@ -7,6 +7,7 @@ namespace legui
         : Container(parent)
     {
         m_padding = 0;
+        m_Vpadding = 0;
     }
     void HorizontalSplitter::setBoundingBox(const sf::FloatRect &box)
     {
@@ -25,7 +26,7 @@ namespace legui
                 m_widgets[i]->updateSize();
                 m_widgets[i]->setBoundingBox(sf::FloatRect(box.left + box.width / 2 + m_padding / 2, box.top + offset, box.width / 2 - m_padding / 2, m_widgets[i]->getBoundingBox().height));
                 //Increase the y offset based on the height of the last right element.
-                offset += m_widgets[i]->getBoundingBox().height;
+                offset += m_widgets[i]->getBoundingBox().height + m_Vpadding;
             }
         }
         m_boundingBox.height = offset;
@@ -46,8 +47,16 @@ namespace legui
     {
         m_padding = padding;
     }
+    void HorizontalSplitter::setVPadding(float Vpadding)
+    {
+        m_Vpadding = Vpadding;
+    }
     float HorizontalSplitter::getPadding()
     {
         return m_padding;
+    }
+    float HorizontalSplitter::getVPadding()
+    {
+        return m_Vpadding;
     }
 }
