ce8aa1a34ac8c00340dccec70978763c09674650
maximaximal <mail@maximaximal.com>
2013-09-07 03:05:09 +0200

    Added the missing sources from the last commit.

--- src/NotificationData.cpp
+++ src/NotificationData.cpp
@@ -2,5 +2,56 @@
 
 namespace legui
 {
+    NotificationData::NotificationData(const std::string &title, const std::string &description)
+    {
+        m_title = title;
+        m_description = description;
+        m_textureNumber = 0;
+    }
+    NotificationData::~NotificationData()
+    {
+    
+    }
 
+    void NotificationData::setTitle(const std::string &value)
+    {
+        m_title = value;
+    }
+    void NotificationData::setDescription(const std::string &value)
+    {
+        m_description = value;
+    }
+    void NotificationData::setIconTexture(const std::string &value)
+    {
+        m_texture = value;
+    }
+    void NotificationData::setIconTextureRect(const std::string &value)
+    {
+        m_textureRect = value;
+    }
+    void NotificationData::setIconTextureNumber(unsigned int number)
+    {
+        m_textureNumber;
+    }
+
+    const std::string& NotificationData::getTitle()
+    {
+        return m_title;
+    }
+    const std::string& NotificationData::getDescription()
+    {
+        return m_description;
+    }
+    const std::string& NotificationData::getIconTexture()
+    {
+        return m_texture;
+    }
+    const std::string& NotificationData::getIconTextureRect()
+    {
+        return m_textureRect;
+    }
+    unsigned int NotificationData::getIconTextureNumber()
+    {
+        return m_textureNumber;
+    }
 }
--- src/NotificationQueue.cpp
+++ src/NotificationQueue.cpp
@@ -2,5 +2,27 @@
 
 namespace legui
 {
+    std::stack<NotificationData> NotificationQueue::m_notifications = std::stack<NotificationData>();
 
+    NotificationQueue::NotificationQueue()
+    {
+        //Nothing to do (Static Class).
+    }
+    NotificationQueue::~NotificationQueue()
+    {
+        //Nothing to do (Static Class).
+    }
+    
+    const NotificationData& NotificationQueue::top()
+    {
+        return m_notifications.top();
+    }
+    void NotificationQueue::pop()
+    {
+        m_notifications.pop();
+    }
+    void NotificationQueue::push(NotificationData &data)
+    {
+        m_notifications.push(data);
+    }
 }
