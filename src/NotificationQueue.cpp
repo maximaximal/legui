a4297000331b125c19b924ee341ae4b5b58e2a38
maximaximal <mail@maximaximal.com>
2013-11-03 00:14:23 +0100

    Worked on the notifications queue.

--- src/NotificationQueue.cpp
+++ src/NotificationQueue.cpp
@@ -2,5 +2,26 @@
 
 namespace legui
 {
+    std::stack<NotificationData> NotificationQueue::m_notifications = std::stack<NotificationData>();
 
+    NotificationQueue::NotificationQueue()
+    {
+        //Nothing to do.
+    }
+    NotificationQueue::~NotificationQueue()
+    {
+        //Nothing to do.
+    }
+    const NotificationData& NotificationQueue::top() 
+    {
+        return m_notifications.top();
+    }
+    void NotificationQueue::pop()
+    {
+        m_notifications.pop();
+    }
+    void NotificationQueue::push(const NotificationData &data)
+    {
+        m_notifications.push(data);
+    }
 }
